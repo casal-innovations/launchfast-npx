da25a28583a909227097b1b14cfce89f475757d9
André Casal <andre@andrecasal.com>
2026-01-21 21:50:25 +0000

    fix: use latest cli package
    Also removed version print.

--- bin/launchfast.js
+++ bin/launchfast.js
@@ -1,12 +1,5 @@
 #!/usr/bin/env node
 
-import { createRequire } from "node:module";
 import { run } from "@launchfasthq/cli";
-
-const require = createRequire(import.meta.url);
-const { version } = require("../package.json");
-
-// Subtle version info for debugging (dimmed)
-console.log(`\x1b[2m@launchfasthq/create v${version}\x1b[0m`);
 
 run();
